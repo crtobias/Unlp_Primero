3e31d5b1b8c9bf75cb7220bb8d3fbda8c533e8dc
crdebian <tgonzalezarriola@gmail.com>
2025-09-09 00:49:33 -0300

    s

--- Pascal/arboles.pas
+++ Pascal/arboles.pas
@@ -3,7 +3,7 @@ Program ArbolesDeLibros;
 type
 
 
-libros = record 
+libro = record 
     nombre:string;
     id:integer;
     precio:integer;
@@ -14,14 +14,60 @@ arbol = ^nodo;
 nodo = record
   ri:arbol;
   le:arbol;
-  dato:libros;
+  dato:libro;
 end;
 
 
-procedure insertarDato();
+procedure insertarDato(var a:arbol ; l:libro);
+var
+  nue:arbol;
 begin
+  if(a = nil) then begin
+    new(nue);
+    nue^.dato:=l;
+    nue^.ri:=nil;
+    nue^.le:=nil;
+    a:=nue;
+  end else begin
 
+    if (l.id > a^.dato.id) then begin
+      insertarDato(a^.ri , l);
+    end else
+      if( l.id < a^.dato.id ) then begin
+        insertarDato(a^.le , l); 
+      end else
+        WriteLn('es repetido');
+
+  end;
 end;
+
+
+procedure leerLibro(var l:libro);
+begin
+  WriteLn('ingresar id');
+  readLn(l.id);
+  if (l.id <> -1) then begin
+    WriteLn('ingresar precio');
+    readLn(l.precio);
+    WriteLn('ingresar nombre');
+    readLn(l.nombre);
+  end;
+end;
+
+// crear arbols mientras libro.id <> -1
+procedure generarArbol(var a:arbol);
+var
+  l:libro;
+begin
+  leerLibro(l);
+  if ( l.id <> -1) then begin
+    insertarDato(a,l);
+    generarArbol(a);
+  end;
+end;
+
+
+
 
 begin
   
