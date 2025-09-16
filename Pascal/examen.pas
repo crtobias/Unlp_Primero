47453e702796caac68a8a321f47fed0b05358905
crdebian <tgonzalezarriola@gmail.com>
2025-09-15 21:17:38 -0300

    asd

--- Pascal/examen.pas
+++ Pascal/examen.pas
@@ -80,8 +80,70 @@ begin
     end;
 end;
 
+function sacarArbolNodosTotal(a:arbol):integer;
+begin
+    if a=nil then begin
+        sacarArbolNodosTotal:=0;
+    end else begin
+        sacarArbolNodosTotal:=sacarArbolNodosTotal(a^.hd)+1+sacarArbolNodosTotal(a^.hi)
+    end
+end;
+// en el array saco cual es mas grande
+function buscarArbolGrande(v:vector):integer;
+var
+    mayor:integer;
+    x,i:integer;
+    memo:integer;
+begin
+    x:=0;
+    for i:=1 to 12 do begin
+        memo:=sacarArbolNodosTotal(v[i]);
+        if memo > x then begin
+            x:= memo;
+            mayor:=i;
+        end;
+    end;
+
+    buscarArbolGrande:=mayor;
+end;
+
+function estaDni(a:arbol; dni:integer):boolean;
+begin
+    if (a = nil) then 
+        estaDni := false
+    else if (a^.dato.dni = dni) then 
+        estaDni := true
+    else if (dni > a^.dato.dni) then 
+        estaDni := estaDni(a^.hd, dni)
+    else 
+        estaDni := estaDni(a^.hi, dni);
+end;
+
+function buscarPaciente(v:vector;dni:integer):boolean;
+var
+    i:integer;
+    esta:boolean;
+begin
+    esta := false;
+    i:=1;
+
+    while (esta=false) and (i<=12) do begin
+        esta:=estaDni(v[i],dni);
+        i:=i+1;
+    end;
+
+    buscarPaciente := esta;
+end;
+
+
 var
     v:vector;
+    mesMayorCant:integer;
+    estaElPaciente:boolean;
 begin 
     crearEstructura(v);
+    mesMayorCant:= buscarArbolGrande(v);
+    writeLn(mesMayorCant);
+    estaElPaciente:=buscarPaciente(v,22);
+    writeLn(estaElPaciente);
 end.
